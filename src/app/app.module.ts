0626545013c1d0b426e175acf9251f3e7a79cd76
Neil Coutinho <njcoutinho@gmail.com>
2020-10-08 11:22:06 +0530

    Completed the basic setup with installation of ajsf and configuration of json-schema-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Bootstrap4FrameworkModule } from '@ajsf/bootstrap4';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Bootstrap4FrameworkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
